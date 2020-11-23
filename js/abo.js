e5e9bba47b9b445212b9d4bc4de72e155a3d2b1f
jmonterrosodev <74883164+jmonterrosodev@users.noreply.github.com>
2020-11-22 18:45:29 -0600

    Web page
    Commit de pagina principal

---
+++ js/abo.js
@@ -0,0 +1,33 @@
+/* offCanvas events */
+UIkit.util.on('#offcanvas-nav-primary', 'beforeshow', function () {
+    // do something
+    console.log('offcanvas beforeshow');
+  });
+  
+  UIkit.util.on('#offcanvas-nav-primary', 'show', function () {
+    // do something
+    console.log('offcanvas show');
+  });
+  
+  UIkit.util.on('#offcanvas-nav-primary', 'shown', function () {
+    // do something
+    console.log('offcanvas shown');
+  });
+  
+  UIkit.util.on('#offcanvas-nav-primary', 'beforehide', function () {
+    // do something
+    console.log('offcanvas beforehide');
+  });
+  
+  UIkit.util.on('#offcanvas-nav-primary', 'hide', function () {
+    // do something
+    console.log('offcanvas hide');
+  });
+  
+  UIkit.util.on('#offcanvas-nav-primary', 'hidden', function () {
+    // do something
+    console.log('offcanvas hidden');
+  });
+  
+  
+  
